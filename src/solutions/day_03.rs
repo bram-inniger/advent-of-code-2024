f006d8df53d5f86458be31bb9b37ae5bc002ae3f
Bram Inniger <bram@inniger.be>
2024-12-03 11:53:25 +0200

    Solved D03P02

--- src/solutions/day_03.rs
+++ src/solutions/day_03.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use regex::Regex;
 
 pub fn solve_1(memory: &str) -> u32 {
@@ -10,6 +11,36 @@ pub fn solve_1(memory: &str) -> u32 {
             a * b
         })
         .sum()
+}
+
+pub fn solve_2(memory: &str) -> u32 {
+    let re_mul = Regex::new(r"mul\((?<a>\d{1,3}),(?<b>\d{1,3})\)").unwrap();
+    let instructions = Regex::new(r"do\(\)|don't\(\)|mul\(\d{1,3},\d{1,3}\)")
+        .unwrap()
+        .find_iter(memory)
+        .map(|m| m.as_str())
+        .collect_vec();
+
+    let mut sum = 0;
+    let mut enabled = true;
+
+    for instruction in instructions {
+        match instruction {
+            "do()" => enabled = true,
+            "don't()" => enabled = false,
+            _ => {
+                if enabled {
+                    let caps = re_mul.captures(instruction).unwrap();
+                    let a = caps.name("a").unwrap().as_str().parse::<u32>().unwrap();
+                    let b = caps.name("b").unwrap().as_str().parse::<u32>().unwrap();
+
+                    sum += a * b;
+                }
+            }
+        }
+    }
+
+    sum
 }
 
 #[cfg(test)]
@@ -28,5 +59,19 @@ mod tests {
         let input = include_str!("../../inputs/day_03.txt").trim();
 
         assert_eq!(183_380_722, solve_1(input));
+    }
+
+    #[test]
+    fn day_03_part_02_sample() {
+        let sample = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+
+        assert_eq!(48, solve_2(sample));
+    }
+
+    #[test]
+    fn day_03_part_02_solution() {
+        let input = include_str!("../../inputs/day_03.txt").trim();
+
+        assert_eq!(82_733_683, solve_2(input));
     }
 }
