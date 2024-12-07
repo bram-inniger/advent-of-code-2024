b529c56990917b380d582579710e39bbc182f460
Bram Inniger <bram@inniger.be>
2024-12-07 19:16:45 +0200

    Updated benchmark for D07P02

--- benches/day_07_bench.rs
+++ benches/day_07_bench.rs
@@ -1,6 +1,7 @@
 use advent_of_code_2024::solutions::*;
 use criterion::{criterion_group, criterion_main, Criterion};
 use itertools::Itertools;
+use std::time::Duration;
 
 /// Run this benchmark using
 /// ```shell
@@ -8,15 +9,15 @@ use itertools::Itertools;
 /// ```
 fn day07(c: &mut Criterion) {
     let input = include_str!("../inputs/day_07.txt").lines().collect_vec();
-    let mut group = c.benchmark_group("day07");
-
-    group.bench_function("part1", |b| {
-        b.iter(|| day_07::solve_1(&input));
-    });
-
-    group.bench_function("part2", |b| {
-        b.iter(|| day_07::solve_2(&input));
-    });
+    c.benchmark_group("day07")
+        .measurement_time(Duration::from_secs(20))
+        .sample_size(500)
+        .bench_function("part1", |b| {
+            b.iter(|| day_07::solve_1(&input));
+        })
+        .bench_function("part2", |b| {
+            b.iter(|| day_07::solve_2(&input));
+        });
 }
 
 criterion_group!(benches, day07);
