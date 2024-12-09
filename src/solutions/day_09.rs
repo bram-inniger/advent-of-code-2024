af575f6803e434f0c5c2c2f4f43a107f0247143f
Bram Inniger <bram@inniger.be>
2024-12-09 16:13:18 +0200

    Solved D09P02

--- src/solutions/day_09.rs
+++ src/solutions/day_09.rs
@@ -37,6 +37,55 @@ pub fn solve_1(disk_map: &str) -> u64 {
     }
 }
 
+pub fn solve_2(disk_map: &str) -> u64 {
+    let mut files: Vec<Block> = Vec::new();
+    let mut spaces: Vec<Block> = Vec::new();
+    let mut file = true;
+    let mut id = 0;
+    let mut idx = 0;
+
+    for d in disk_map.chars().map(|c| c.to_digit(10).unwrap() as usize) {
+        if file {
+            let file = Block { idx, len: d, id };
+
+            files.push(file);
+            id += 1;
+        } else {
+            let space = Block { idx, len: d, id: 0 };
+
+            spaces.push(space);
+        }
+
+        idx += d;
+        file = !file;
+    }
+
+    for file in files.iter_mut().rev() {
+        if let Some(space) = spaces
+            .iter_mut()
+            .take_while(|s| s.idx < file.idx)
+            .find(|s| s.len >= file.len)
+        {
+            file.idx = space.idx;
+            space.idx += file.len;
+            space.len -= file.len;
+        }
+    }
+
+    files
+        .iter()
+        .flat_map(|f| (f.idx..f.idx + f.len).map(|idx| f.id * idx as u64))
+        .sum()
+}
+
+// improvement idea, make enum for "File" and "Empty" ?
+#[derive(Debug, Clone, Copy, Ord, PartialOrd, Eq, PartialEq, Hash)]
+struct Block {
+    idx: usize,
+    len: usize,
+    id: u64,
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -53,5 +102,19 @@ mod tests {
         let input = include_str!("../../inputs/day_09.txt").trim();
 
         assert_eq!(6_201_130_364_722, solve_1(input));
+    }
+
+    #[test]
+    fn day_09_part_02_sample() {
+        let sample = "2333133121414131402";
+
+        assert_eq!(2_858, solve_2(sample));
+    }
+
+    #[test]
+    fn day_09_part_02_solution() {
+        let input = include_str!("../../inputs/day_09.txt").trim();
+
+        assert_eq!(6_221_662_795_602, solve_2(input));
     }
 }
