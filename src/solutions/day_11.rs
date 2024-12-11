8e4bd118a8a6f091732779b44d93251d6abc2dfb
Bram Inniger <bram@inniger.be>
2024-12-11 12:44:00 +0200

    Solved D11P02

--- src/solutions/day_11.rs
+++ src/solutions/day_11.rs
@@ -1,28 +1,43 @@
-pub fn solve_1(stones: &str) -> usize {
-    let mut stones: Vec<_> = stones
+use itertools::Itertools;
+use rustc_hash::FxHashMap;
+
+pub fn solve_1(stones: &str) -> u64 {
+    solve(stones, 25)
+}
+
+pub fn solve_2(stones: &str) -> u64 {
+    solve(stones, 75)
+}
+
+fn solve(stones: &str, iterations: u32) -> u64 {
+    let mut stones: FxHashMap<u64, u64> = stones
         .split(" ")
         .map(|s| s.parse::<u64>().unwrap())
+        .sorted()
+        .chunk_by(|&stone| stone)
+        .into_iter()
+        .map(|(stone, grouped)| (stone, grouped.count() as u64))
         .collect();
 
-    for _ in 0..25 {
-        let mut new_stones: Vec<u64> = Vec::new();
+    for _ in 0..iterations {
+        let mut new_stones: FxHashMap<u64, u64> = FxHashMap::default();
 
-        for stone in stones {
+        for (stone, count) in stones {
             if stone == 0 {
-                new_stones.push(1);
+                *new_stones.entry(1).or_insert(0) += count;
             } else if nr_digits(stone) % 2 == 0 {
                 let (left, right) = split_stone(stone);
-                new_stones.push(left);
-                new_stones.push(right);
+                *new_stones.entry(left).or_insert(0) += count;
+                *new_stones.entry(right).or_insert(0) += count;
             } else {
-                new_stones.push(stone * 2024)
+                *new_stones.entry(stone * 2024).or_insert(0) += count;
             }
         }
 
         stones = new_stones;
     }
 
-    stones.len()
+    stones.values().sum()
 }
 
 fn nr_digits(n: u64) -> u32 {
@@ -73,5 +88,17 @@ mod tests {
         let input = include_str!("../../inputs/day_11.txt").trim();
 
         assert_eq!(183_248, solve_1(input));
+    }
+
+    #[test]
+    fn day_11_part_02_sample() {
+        // No sample input provided
+    }
+
+    #[test]
+    fn day_11_part_02_solution() {
+        let input = include_str!("../../inputs/day_11.txt").trim();
+
+        assert_eq!(218_811_774_248_729, solve_2(input));
     }
 }
