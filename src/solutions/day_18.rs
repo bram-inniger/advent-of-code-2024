2c9f364d87e916342375d1be699ab26be5ebee87
Bram Inniger <bram@inniger.be>
2024-12-18 12:10:55 +0200

    Solved D18P02

--- src/solutions/day_18.rs
+++ src/solutions/day_18.rs
@@ -1,8 +1,19 @@
-use rustc_hash::{FxHashMap, FxHashSet};
+use rustc_hash::FxHashSet;
 use std::collections::VecDeque;
 use std::ops::Not;
 
 pub fn solve_1(bytes: &[&str], nr_bytes: usize, max_dim: i32) -> u32 {
+    distance_to_exit(bytes, nr_bytes, max_dim).unwrap()
+}
+
+pub fn solve_2(bytes: &[&str], max_dim: i32) -> String {
+    (0..)
+        .find(|&nr_bytes| distance_to_exit(bytes, nr_bytes, max_dim).is_none())
+        .map(|nr_bytes| bytes[nr_bytes - 1].to_owned())
+        .unwrap()
+}
+
+fn distance_to_exit(bytes: &[&str], nr_bytes: usize, max_dim: i32) -> Option<u32> {
     let corruption: FxHashSet<_> = bytes
         .iter()
         .take(nr_bytes)
@@ -17,10 +28,14 @@ pub fn solve_1(bytes: &[&str], nr_bytes: usize, max_dim: i32) -> u32 {
 
     let mut to_visit = VecDeque::<(Position, u32)>::new();
     to_visit.push_back((start, 0));
-    let mut visited = FxHashMap::<Position, u32>::default();
+    let mut visited = FxHashSet::<Position>::default();
 
     while let Some((position, distance)) = to_visit.pop_front() {
-        if visited.contains_key(&position)
+        if position == end {
+            return Some(distance);
+        }
+
+        if visited.contains(&position)
             || corruption.contains(&position)
             || (0..=max_dim).contains(&position.x).not()
             || (0..=max_dim).contains(&position.y).not()
@@ -28,7 +43,7 @@ pub fn solve_1(bytes: &[&str], nr_bytes: usize, max_dim: i32) -> u32 {
             continue;
         }
 
-        visited.insert(position, distance);
+        visited.insert(position);
         [(1, 0), (-1, 0), (0, 1), (0, -1)]
             .map(|(dx, dy)| Position {
                 x: position.x + dx,
@@ -38,7 +53,7 @@ pub fn solve_1(bytes: &[&str], nr_bytes: usize, max_dim: i32) -> u32 {
             .for_each(|neighbour| to_visit.push_back((neighbour, distance + 1)));
     }
 
-    visited[&end]
+    None
 }
 
 #[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash)]
@@ -104,5 +119,48 @@ mod tests {
             .collect_vec();
 
         assert_eq!(306, solve_1(&input, 1024, 70));
+    }
+
+    #[test]
+    fn day_18_part_02_sample() {
+        #[rustfmt::skip]
+        let sample = vec![
+            "5,4",
+            "4,2",
+            "4,5",
+            "3,0",
+            "2,1",
+            "6,3",
+            "2,4",
+            "1,5",
+            "0,6",
+            "3,3",
+            "2,6",
+            "5,1",
+            "1,2",
+            "5,5",
+            "2,5",
+            "6,5",
+            "1,4",
+            "0,4",
+            "6,4",
+            "1,1",
+            "6,1",
+            "1,0",
+            "0,5",
+            "1,6",
+            "2,0",
+        ];
+
+        assert_eq!("6,1", solve_2(&sample, 6));
+    }
+
+    #[test]
+    fn day_18_part_02_solution() {
+        let input = include_str!("../../inputs/day_18.txt")
+            .lines()
+            .collect_vec();
+
+        assert_eq!("38,63", solve_2(&input, 70));
     }
 }
