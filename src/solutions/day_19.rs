0562129298cc7effeca6c848c156706a4b5b8bc2
Bram Inniger <bram@inniger.be>
2024-12-19 12:24:57 +0200

    Improved D19P02

--- src/solutions/day_19.rs
+++ src/solutions/day_19.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+use rustc_hash::FxHashMap;
 
 pub fn solve_1(towels: &[&str]) -> usize {
     let patterns = towels[0].split(", ").collect_vec();
@@ -6,16 +7,38 @@ pub fn solve_1(towels: &[&str]) -> usize {
 
     designs
         .iter()
-        .filter(|design| is_valid(design, &patterns))
+        .filter(|design| ways_count(design, &patterns, &mut FxHashMap::default()) > 0)
         .count()
 }
 
-fn is_valid(design: &str, patterns: &[&str]) -> bool {
-    design.is_empty()
-        || patterns
+pub fn solve_2(towels: &[&str]) -> u64 {
+    let patterns = towels[0].split(", ").collect_vec();
+    let designs = towels.iter().skip(2).copied().collect_vec();
+
+    designs
+        .iter()
+        .map(|design| ways_count(design, &patterns, &mut FxHashMap::default()))
+        .sum()
+}
+
+fn ways_count(design: &str, patterns: &[&str], cache: &mut FxHashMap<usize, u64>) -> u64 {
+    if let Some(count) = cache.get(&design.len()) {
+        return *count;
+    }
+
+    let count = if design.is_empty() {
+        1
+    } else {
+        patterns
             .iter()
             .filter(|&pattern| design.starts_with(pattern))
-            .any(|&pattern| is_valid(&design[pattern.len()..], patterns))
+            .map(|&pattern| ways_count(&design[pattern.len()..], patterns, cache))
+            .sum()
+    };
+
+    cache.insert(design.len(), count);
+
+    count
 }
 
 #[cfg(test)]
@@ -49,5 +72,32 @@ mod tests {
             .collect_vec();
 
         assert_eq!(220, solve_1(&input));
+    }
+
+    #[test]
+    fn day_19_part_02_sample() {
+        let sample = vec![
+            "r, wr, b, g, bwu, rb, gb, br",
+            "",
+            "brwrr",
+            "bggr",
+            "gbbr",
+            "rrbgbr",
+            "ubwu",
+            "bwurrg",
+            "brgr",
+            "bbrgwb",
+        ];
+
+        assert_eq!(16, solve_2(&sample));
+    }
+
+    #[test]
+    fn day_19_part_02_solution() {
+        let input = include_str!("../../inputs/day_19.txt")
+            .lines()
+            .collect_vec();
+
+        assert_eq!(565_600_047_715_343, solve_2(&input));
     }
 }
