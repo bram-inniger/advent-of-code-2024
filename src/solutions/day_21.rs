69bfb209022e89b5694824c070552eb38ac4619d
Bram Inniger <bram@inniger.be>
2024-12-22 01:16:41 +0200

    Solved D21P02

--- src/solutions/day_21.rs
+++ src/solutions/day_21.rs
@@ -4,33 +4,105 @@ use std::fmt;
 use std::fmt::Display;
 use std::ops::Not;
 
-pub fn solve_1(codes: &[&str]) -> usize {
-    codes.iter().map(|code| complexity(code)).sum()
+pub fn solve_1(codes: &[&str]) -> u64 {
+    solve(codes, 2)
 }
 
-fn complexity(code: &str) -> usize {
+pub fn solve_2(codes: &[&str]) -> u64 {
+    solve(codes, 25)
+}
+
+fn solve(codes: &[&str], nr_robots: u32) -> u64 {
     let numeric = Keypad::numeric();
     let directional = Keypad::directional();
+    let mut cache = FxHashMap::default();
 
-    let mut paths = vec![code.chars().map(Button::from).collect_vec()];
-    for keypad in [&numeric, &directional, &directional] {
-        paths = paths
-            .iter()
-            .flat_map(|path| shortest_len(path, keypad).paths)
-            .collect_vec();
-    }
+    codes
+        .iter()
+        .map(|code| complexity(code, nr_robots, &numeric, &directional, &mut cache))
+        .sum()
+}
 
-    let shortest = paths.iter().map(|path| path.len()).min().unwrap();
-    let numeric_part = code[..code.len() - 1].parse::<usize>().unwrap();
+fn complexity(
+    code: &str,
+    nr_robots: u32,
+    numeric: &Keypad,
+    directional: &Keypad,
+    cache: &mut FxHashMap<(Vec<Button>, u32), u64>,
+) -> u64 {
+    let shortest = to_next_layer(&code.chars().map(Button::from).collect_vec(), numeric)
+        .paths
+        .iter()
+        .map(|path| split_sub_paths(path))
+        .map(|sub_paths| {
+            sub_paths
+                .iter()
+                .map(|sub_path| shortest_len(sub_path, nr_robots, directional, cache))
+                .sum::<u64>()
+        })
+        .min()
+        .unwrap();
+    let numeric_part = code[..code.len() - 1].parse::<u64>().unwrap();
 
     shortest * numeric_part
 }
 
-fn shortest_len(buttons: &[Button], keypad: &Keypad) -> Paths {
+fn shortest_len(
+    path: &[Button],
+    level: u32,
+    keypad: &Keypad,
+    cache: &mut FxHashMap<(Vec<Button>, u32), u64>,
+) -> u64 {
+    if cache.contains_key(&(path.to_vec(), level)) {
+        return cache[&(path.to_vec(), level)];
+    }
+
+    let len = if level == 0 {
+        path.len() as u64
+    } else {
+        let next_paths = to_next_layer(path, keypad).paths;
+
+        next_paths
+            .iter()
+            .map(|next_path| {
+                split_sub_paths(next_path)
+                    .iter()
+                    .map(|sub_path| shortest_len(sub_path, level - 1, keypad, cache))
+                    .sum::<u64>()
+            })
+            .min()
+            .unwrap()
+    };
+
+    cache.insert((path.to_vec(), level), len);
+
+    len
+}
+
+fn split_sub_paths(path: &[Button]) -> Vec<Vec<Button>> {
+    let a_indices = path
+        .iter()
+        .enumerate()
+        .filter(|(_, button)| **button == Button::Activate)
+        .map(|(idx, _)| idx)
+        .collect_vec();
+
+    (0..a_indices.len())
+        .map(|idx| {
+            (
+                if idx == 0 { 0 } else { a_indices[idx - 1] + 1 },
+                a_indices[idx],
+            )
+        })
+        .map(|(from, to)| path[from..=to].to_vec())
+        .collect_vec()
+}
+
+fn to_next_layer(path: &[Button], keypad: &Keypad) -> Paths {
     let mut paths = vec![vec![]];
     let mut from = Button::Activate;
 
-    for to in buttons {
+    for to in path {
         paths = paths
             .iter()
             .flat_map(|first| {
@@ -325,9 +397,9 @@ impl Paths {
     }
 
     fn keep_shortest_compressed(&self) -> Self {
-        fn compressed_len(path: &[Button]) -> usize {
+        fn compressed_len(path: &[Button]) -> u64 {
             if path.len() <= 1 {
-                return path.len();
+                return path.len() as u64;
             }
 
             let mut len = 1;
@@ -395,5 +467,19 @@ mod tests {
             .collect_vec();
 
         assert_eq!(211_930, solve_1(&input));
+    }
+
+    #[test]
+    fn day_21_part_02_sample() {
+        // No sample input provided
+    }
+
+    #[test]
+    fn day_21_part_02_solution() {
+        let input = include_str!("../../inputs/day_21.txt")
+            .lines()
+            .collect_vec();
+
+        assert_eq!(263_492_840_501_566, solve_2(&input));
     }
 }
